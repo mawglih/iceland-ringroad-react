a7259ebba801678f746a8d876560e81dc2d170db
Oleg Markoff <mawglih@gmail.com>
2017-12-05 16:49:47 -0500

    git:checkout

--- src/components/about.js
+++ src/components/about.js
@@ -5,13 +5,23 @@ class About extends Component {
         return(
             <div>
                 <section className="section-about">
-                    <div className="u-center-text u-margin-bottom-8">
+                    <div className="u-center-text u-margin-bottom-big">
                         <h2 className="heading-secondary">
                             Exciting road trip for adventurous people
                         </h2>
                     </div>
                     <div className="row">
-                        <div className="col-1-of-2">Text</div>
+                        <div className="col-1-of-2">
+                            <h3 className="heading-tertiary u-margin-bottom-small">You are going to fall in love with icelandic views</h3>
+                            <p className="paragraph">
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum, optio saepe natus eum dolor excepturi. Accusamus iste itaque atque dicta quo sed veritatis consequuntur non voluptate, dignissimos culpa laudantium mollitia.
+                            </p>
+                            <h3 className="heading-tertiary u-margin-bottom-small">Have an adventure of a lifetime</h3>
+                            <p className="paragraph">
+                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit ut corporis et soluta vitae, nisi delectus, fuga quaerat hic nostrum rem fugit quas beatae consequatur sapiente doloribus quis perspiciatis modi? Heritatis consequuntur non voluptate, dignissimos culpa laudantium mollitia.
+                            </p>
+                                <a  className="btn-text" href="#">Learn more &rarr;</a>
+                        </div>
                         <div className="col-1-of-2">Image</div>
                     </div>
                 </section>
